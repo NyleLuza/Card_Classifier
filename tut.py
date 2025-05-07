f285ab32fff4209cc1c216b2c34d1628ce7ccaad
Jluza <Jluza21@gmail.com>
2025-05-07 15:22:09 +0900

    first committ

---
+++ tut.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import dataset, dataloader
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
