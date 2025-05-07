1c4a94f1277b37c8dd62d050d14971d7fb953bda
Jluza <Jluza21@gmail.com>
2025-05-07 15:30:34 +0900

    added dataset class

--- tut.py
+++ tut.py
@@ -8,3 +8,21 @@ from torchvision.datasets import ImageFolder
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+class PlayingCardDataset(dataset):
+    # needs to know what to do when created
+    def __init__(self, data_dir, transform=None):
+        # utlizie imagefolder function to obtain the data from the data directory. Handles creating labels for us
+        self.data = ImageFolder(data_dir, transform)
+    
+    # dataloader will need to know how many examples we have in a dataset
+    def __len__(self):
+        return len(self.data)
+
+    # returns an item from the dataset
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    # returns the classes from the data folder
+    def classes(self):
+        return self.data.classes
