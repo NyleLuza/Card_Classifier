878f0ceea74ed624b01998d043b17b379b963ec7
Jluza <Jluza21@gmail.com>
2025-05-08 17:01:22 +0900

    made changes to import

--- tut.py
+++ tut.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import dataset, dataloader
+from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
+import timm
 
 import matplotlib.pyplot as plt
 import pandas as pd
